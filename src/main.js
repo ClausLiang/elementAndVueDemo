b59b670ee38f2706db2efebfa306be92a28e4e1d
梁永刚 <1475524294@qq.com>
2021-12-17 14:00:25 +0800

    把请求抽离出来

--- src/main.js
+++ src/main.js
@@ -9,46 +9,8 @@ import store from '@/store/store'
 import 'cropperjs/dist/cropper.css'
 import './iconfont/iconfont.css' // 引入iconfont
 import loading from './components/common/loading.vue'
-import axios from 'axios'
-axios.defaults.retry = 4
-axios.defaults.retryDelay = 5000
-axios.interceptors.response.use(undefined, (err) => {
-    console.log('err:', err)
-    console.log('errConfig:', err.config)
-    console.log('errResponse:', err.response)
-    if (err.response) {
-        switch (err.response.status) {
-            case 404:
-                // alert('找不见接口')
-                ElementUI.Message.error('找不见接口')
-                return Promise.reject(err)
-        }
-    }
-    var config = err.config
-    // If config does not exist or the retry option is not set, reject
-    if (!config || !config.retry) return Promise.reject(err)
-    // Set the variable for keeping track of the retry count
-    config.__retryCount = config.__retryCount || 0
-    // Check if we've maxed out the total number of retries
-    if (config.__retryCount >= config.retry) {
-        // Reject with the error
-        return Promise.reject(err)
-    }
-    // Increase the retry count
-    config.__retryCount += 1
-    // Create new promise to handle exponential backoff
-    var backoff = new Promise(function (resolve) {
-        setTimeout(function () {
-            resolve()
-        }, config.retryDelay || 1)
-    })
-    // Return the promise in which recalls axios to retry the request
-    return backoff.then(function () {
-        return axios(config)
-    })
-})
+import '@/common/request' // axios
 Vue.component('loading', loading)
-Vue.prototype.axios = axios
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
